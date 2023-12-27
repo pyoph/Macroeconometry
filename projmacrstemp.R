e626a3ac4202df560c7f8e589f5c5f7873721362
pyoph <115776656+pyoph@users.noreply.github.com>
2023-12-27 22:52:51 +0100

    Add files via upload
    Code supplémentaire sur le fichier R (tentative de simuler les priors)
    Fichier de données de réserves

--- projmacrstemp.R
+++ projmacrstemp.R
@@ -1,6 +1,7 @@
 #install.packages("usethis")
 #install.packages("brms")
 #install.packages("MCMCpack")
+#install.packages("fastmatrix")
 library(ggplot2)
 library(gridExtra)
 library(zoo)
@@ -8,6 +9,7 @@ library(brms)
 library(MASS)
 library(MCMCpack)
 library(vars)
+library(fastmatrix)
 #usethis::use_git_config(user.name="pyoph", user.email="paul.guillot2@gmail.com")
 
 #Simulation of normal prior
@@ -28,6 +30,8 @@ invWishart <- function(A,df){
 fichierTL <- read.csv("TLAACBW027SBOG.csv")
 
 fichierFFR <- read.csv("EFFR.csv")
+
+fileReserves <- read.csv("WRESBAL.csv")
 
 #Removing of NA's
 fichierFFR <- na.omit(fichierFFR)
@@ -50,50 +54,6 @@ TLAAj <- data.frame(Date = dates_journalieres, reserves = TLAACts)
 TLAAj
 fichierFFR$Spread
 mergedData <- merge(TLAAj,fichierFFR,by = "Date")
-
-###Variables###
-
-#Data vector
-y <- cbind(mergedData$reserves,mergedData$Spread)
-colnames(y) <- c("reserves","spread")
-
-##presample
-mergedDataps <- mergedData[mergedData$Date <= "2010-01-19",]
-yps <- cbind(as.numeric(mergedDataps$reserves),as.numeric(mergedDataps$Spread))
-colnames(yps) <- c("reserves","spread")
-typeof(mergedDataps$Spread)
-as.double(mergedDataps$Spread)
-mergedDataps$Spread
-#Parameters
-m = 10 #number of lags
-
-sigma1 = 0.5
-sigma2 = 0.8
-alpha21 = 0.9
-
-l1 = 0.04
-l2 = 0.2
-l3 = 0.01
-
-sigma_t <- diag(c(sigma1, sigma2))
-
-A_t <- matrix(c(1, 0, alpha21, 1), ncol = 2)
-
-##Setting of B^, alpha^, log(sigma^), PsiB^ and Psialpha^ by OLS###
-
-#Regression on datas from 01/07/2009 to 19/01/2010
-mergedDataps <- na.omit(mergedDataps)
-
-
-modelOLSps <- lm(as.double(Spread) ~ as.double(reserves), data = mergedDataps)
-summary(modelOLSps)
-
-#Recuperation of B^
-
-B0 <- coef(modelOLSps)[2]
-B0
-
-sigma0 <- vcov(modelOLSps)
 
 ###Representation of datas###
 
@@ -120,4 +80,72 @@ grid.arrange(plot1, plot2, ncol = 2)
 
 
 
+###Variables###
 
+#Data vector
+y <- cbind(mergedData$reserves,mergedData$Spread)
+colnames(y) <- c("reserves","spread")
+
+##presample
+mergedDataps <- mergedData[mergedData$Date <= "2010-01-19",]
+yps <- cbind(as.numeric(mergedDataps$reserves),as.numeric(mergedDataps$Spread))
+colnames(yps) <- c("reserves","spread")
+typeof(mergedDataps$Spread)
+as.double(mergedDataps$Spread)
+mergedDataps$Spread
+#Parameters
+m = 10 #number of lags
+
+
+l1 = 0.04
+l2 = 0.2
+l3 = 0.01
+
+
+
+
+##Setting of B^, alpha^, log(sigma^), PsiB^ and Psialpha^ by OLS###
+
+#Regression on datas from 01/07/2009 to 19/01/2010
+mergedDataps <- na.omit(mergedDataps)
+
+
+modelOLSps <- lm(as.double(Spread) ~ as.double(reserves), data = mergedDataps)
+summary(modelOLSps)
+
+#Recuperation of B^
+
+Bhat <- coef(modelOLSps)[2]
+Bhat
+
+Omega0 <- vcov(modelOLSps)
+Omega0
+
+
+#Extraction of PsiA and PsiB
+PsiA <- Omega0[1,1]
+PsiB <- Omega0[2,2]
+
+#LDL décompostion of Sigma0*t(Sigma0)
+
+A0 <- ldl(Omega0*t(Sigma0))[2]
+
+#Extraction of sigma10 and sigma20
+str <- as.character(A0[1])
+sigma10 <- as.double(substr(str[1],3,8))
+sigma10
+sigma20 <- as.double(substr(str[1],23,32))
+sigma20 <- sigma20 * 10^(-23)
+sigma20
+
+#Extration of a10
+Sigma0 <- ldl(Omega0*t(Sigma0))[1] 
+str <- as.character(Sigma0[1])
+a10 <- as.double(substr(str,6,12))*10^(-9)
+a10
+
+#Settings of priors
+set.seed(13579)                        # Set seed
+
+B0 <- rnorm(1,Bhat,PsiB)
+B0
